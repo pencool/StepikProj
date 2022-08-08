ed72c9d41dd668fa47dd5be54b725e168ecd2023
grizly <pencool2@yandex.ru>
2022-08-08 23:58:48 +0300

    123

--- StepikProfessionalCourse/3-3-19.py
+++ StepikProfessionalCourse/3-3-19.py
@@ -5,22 +5,21 @@ def is_available_date(dates, some_date):
     answer = []
     for i in range(len(dates)):
         if len(dates[i]) == 10:
-            dates[i] = {int(datetime.strptime(dates[i], '%d.%m.%Y').timestamp())}
+            dates[i] = {int(datetime.strptime(dates[i], '%d.%m.%Y').toordinal())}
         else:
-            dates[i] = {j for j in range(int(datetime.strptime(dates[i].split('-')[0], '%d.%m.%Y').timestamp()),
-                       int(datetime.strptime(dates[i].split('-')[1], '%d.%m.%Y').timestamp()))}
+            dates[i] = {j for j in range(int(datetime.strptime(dates[i].split('-')[0], '%d.%m.%Y').toordinal()),
+                       int(datetime.strptime(dates[i].split('-')[1], '%d.%m.%Y').toordinal())+1)}
     if len(some_date) == 10:
-        some_date = {int(datetime.strptime(some_date, '%d.%m.%Y').timestamp())}
+        some_date = {int(datetime.strptime(some_date, '%d.%m.%Y').toordinal())}
     else:
-        some_date = {i for i in range(int(datetime.strptime(some_date.split('-')[0], '%d.%m.%Y').timestamp()),
-                   int(datetime.strptime(some_date.split('-')[1], '%d.%m.%Y').timestamp()))}
+        some_date = {i for i in range(int(datetime.strptime(some_date.split('-')[0], '%d.%m.%Y').toordinal()),
+                   int(datetime.strptime(some_date.split('-')[1], '%d.%m.%Y').toordinal())+1)}
     for i in dates:
         if len(some_date & i) == 0:
             answer.append(True)
         else:
             answer.append(False)
-    print(answer)
-    return True if True in answer else False
+    return all(answer)
 
 
 dates = ['04.11.2021', '05.11.2021-09.11.2021']
