3db4f3650c84d1e1e8de6955fc787ca6f3640102
Павел Корчилов <pencool2@yandex.ru>
2023-09-28 15:17:01 +0300

    123

--- StepikProfessionalCourse/9-7-24.py
+++ StepikProfessionalCourse/9-7-24.py
@@ -5,13 +5,21 @@ def takes_positive(func):
                 raise TypeError
             elif i <= 0:
                 raise ValueError
+        for k, v in kwargs.items():
+            if type(v) is not int:
+                raise TypeError
+            elif v <= 0:
+                raise ValueError
         return func(*args, **kwargs)
     return wrapper
 
 
 @takes_positive
-def positive_sum(*args):
-    return sum(args)
+def positive_sum(*args, **kwargs):
+    return sum(args) + sum(kwargs.values())
 
 
-print(positive_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+try:
+    print(positive_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, par1=1, sep=-40))
+except Exception as err:
+    print(type(err))
